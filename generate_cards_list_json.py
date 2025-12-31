55e1712c935fa2b8082a43dcc41b98c4556d3296
John Wilkes <jwilkes0011@gmail.com>
2025-12-30 23:37:41 -0600

    Organizing card lists

--- generate_cards_list_json.py
+++ generate_cards_list_json.py
@@ -2,6 +2,70 @@ from html.parser import HTMLParser
 import json
 from typing import Any
 
+def create_card() -> dict[str, Any]:
+    return {
+        'Name': '',
+        'Set': '',
+        'Types': [],
+    }
+
+BASIC_CARD_NAMES = {
+    'Copper',
+    'Silver',
+    'Gold',
+    'Platinum',
+    'Potion',
+    'Curse',
+    'Estate',
+    'Duchy',
+    'Province',
+}
+
+def is_basic_card(card: dict[str, Any]) -> bool:
+    return card['Name'] in BASIC_CARD_NAMES
+
+def is_non_supply_card(card: dict[str, Any]) -> bool:
+    if card['Name'] in {'Horse'}:
+        return True
+
+    for t in card['Types']:
+        if t in {'Loot', 'Prize', 'Reward', 'Spirit', 'Zombie'}:
+            return True
+
+    return False
+
+VALID_KINGDOM_CARD_TYPES = {
+    'Action',
+    'Attack',
+    'Augur',
+    'Castle',
+    'Clash',
+    'Command',
+    'Doom',
+    'Duration',
+    'Fate',
+    'Fort',
+    'Gathering',
+    'Knight',
+    'Liaison',
+    'Looter',
+    'Night',
+    'Odyssey',
+    'Omen',
+    'Reaction',
+    'Reserve',
+    'Shadow',
+    'Townsfolk',
+    'Traveller',
+    'Treasure',
+    'Victory',
+    'Wizard',
+}
+
+def assert_kingdom_card_types(types: list[str]) -> None:
+    for t in types:
+        assert t in VALID_KINGDOM_CARD_TYPES, f'{t} is not a valid kingdom card type'
+
 class DominionCardsParser(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -11,8 +75,26 @@ class DominionCardsParser(HTMLParser):
         self.is_header_row = True
         self.column_index = 0
         self.headers: list[str] = []
-        self.current_card = {}
-        self.cards: list[dict[str, Any]] = []
+        self.current_card = create_card()
+        self.cards: dict[str, list[dict[str, Any]]] = {
+            'Basic': [],
+            'Kingdom': [],
+            'NonSupply': [],
+            'Shelters': [],
+            'Ruins': [],
+            'Events': [],
+            'Landmarks': [],
+            'Heirlooms': [],
+            'Boons': [],
+            'Hexes': [],
+            'States': [],
+            'Projects': [],
+            'Artifacts': [],
+            'Ways': [],
+            'Allies': [],
+            'Traits': [],
+            'Prophecies': [],
+        }
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
         if tag == 'table':
@@ -30,8 +112,42 @@ class DominionCardsParser(HTMLParser):
             if self.is_header_row:
                 self.is_header_row = False
             else:
-                self.cards.append(self.current_card)
-                self.current_card = {}
+                if is_basic_card(self.current_card):
+                    self.cards['Basic'].append(self.current_card)
+                elif is_non_supply_card(self.current_card):
+                    self.cards['NonSupply'].append(self.current_card)
+                elif 'Shelter' in self.current_card['Types']:
+                    self.cards['Shelters'].append(self.current_card)
+                elif 'Ruins' in self.current_card['Types']:
+                    self.cards['Ruins'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Event':
+                    self.cards['Events'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Landmark':
+                    self.cards['Landmarks'].append(self.current_card)
+                elif 'Heirloom' in self.current_card['Types']:
+                    self.cards['Heirlooms'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Boon':
+                    self.cards['Boons'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Hex':
+                    self.cards['Hexes'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'State':
+                    self.cards['States'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Project':
+                    self.cards['Projects'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Artifact':
+                    self.cards['Artifacts'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Way':
+                    self.cards['Ways'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Ally':
+                    self.cards['Allies'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Trait':
+                    self.cards['Traits'].append(self.current_card)
+                elif self.current_card['Types'][0] == 'Prophecy':
+                    self.cards['Prophecies'].append(self.current_card)
+                else:
+                    assert_kingdom_card_types(self.current_card['Types'])
+                    self.cards['Kingdom'].append(self.current_card)
+                self.current_card = create_card()
         elif tag == 'th':
             self.parsing_th = False
             self.column_index += 1
@@ -45,6 +161,8 @@ class DominionCardsParser(HTMLParser):
             match column_name:
                 case 'Name':
                     self.current_card['Name'] = data.strip()
+                case 'Set':
+                    self.current_card['Set'] += data
                 case 'Types':
                     self.current_card['Types'] = [t.strip() for t in data.split('-')]
         elif self.parsing_th:
